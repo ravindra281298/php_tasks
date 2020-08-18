d04520119ee00d75b9be0ca2d14c7ca0ce6bdcd5
ravindra281298 <ravindra281298@gmail.com>
2020-08-19 00:13:14 +0530

    made changes

--- Calculator.php
+++ Calculator.php
@@ -1,36 +1,64 @@
 <?php
 
-    function addNumbers($a, $b) {
-        return $a + $b;
+    class calculator {
+
+        public $number1,$number2;
+
+        function getInput() {
+            $this->number1 = readline("Enter first Number: ");
+            $this->number2 = readline("Enter second Number: ");
+        }
+
+        function addNumbers() {
+            $this->getInput();
+            return $this->number1."+".$this->number2."=".($this->number1 + $this->number2);
+        }
+
+        function subtractNumbers() {
+            $this->getInput();
+            return $this->number1."-".$this->number2."=".($this->number1 - $this->number2);
+        }
+
+        function multiplyNumbers() {
+            $this->getInput();
+            return $this->number1."*".$this->number2."=".($this->number1 * $this->number2);
+        }
+
+        function divideNumbers() {
+            $this->getInput();
+            if($this->number2 == 0) {
+                return "Divisor cannot be zero!";
+            }
+            return $this->number1."/".$this->number2."=".($this->number1 / $this->number2);
+        }
     }
 
-    function subtractNumbers($a, $b) {
-        return $a - $b;
+    $flag = 1;
+    while($flag==1) {
+
+        echo "Calculator!\n 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division\n ";
+        $choice = (int)readline("Enter your choice(1-4): ");
+
+        $object = new calculator();
+
+        switch($choice) {
+            case 1: print $object->addNumbers();
+            break;
+            case 2: print $object->subtractNumbers();
+            break;
+            case 3: print $object->multiplyNumbers();
+            break;
+            case 4: print $object->divideNumbers();
+            break;
+            default: print " Please Enter correct choice!!";
+        }
+
+        echo "\n";
+        $choice =  readline("press 1 to continue and 0 to exit : ");
+        if($choice == 0) {
+            $flag=0;
+        }
     }
 
-    function multiplyNumbers($a, $b) {
-        return $a * $b;
-    }
-
-    function divideNumbers($a, $b) {
-        return $a / $b;
-    }
-
-    echo "Calculator!\n 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division\n ";
-    $n = (int)readline("Enter your choice(1-4): ");
-    $number1 = readline("Enter first Number: ");
-    $number2 = readline("Enter second Number: ");
-
-    switch($n) {
-        case 1: print addNumbers($number1,$number2);
-        break;
-        case 2: print subtractNumbers($number1,$number2);
-        break;
-        case 3: print multiplyNumbers($number1,$number2);
-        break;
-        case 4: print divideNumbers($number1,$number2);
-        break;
-        default: print "Enter correct choice!!";
-    }
 
 ?>
